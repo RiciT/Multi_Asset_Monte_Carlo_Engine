9228e9b3a37f3d5e99153ee3a2c605ed2e6eee66
RiCiT <tamas.balint.farago@gmail.com>
2026-02-20 15:37:07 +0100

    refactored again as yahoo finance was giving 401 but it woeks flawlessly now with stooq

--- data_gathering/DataGathering.jl
+++ data_gathering/DataGathering.jl
@@ -3,42 +3,37 @@ using Downloads, CSV, DataFrames, Statistics, Dates, LinearAlgebra
 # find some more interesting ones later but now sticking with FAANG
 tickers = ["AAPL", "MSFT", "NVDA", "GOOGL", "AMZN", "META"]
 
-stopTime = Int(floor(datetime2unix(now())))
-startTime = Int(floor(datetime2unix(now() - Year(1))))
-
-function fetchYahooData(ticker, t1, t2)
-    url = "https://query1.finance.yahoo.com/v7/finance/download/$ticker?period1=$t1&period2=$t2&interval=1d&events=history&includeAdjustedClose=true"
-    
+function fetchStooqData(ticker)
+    url = "https://stooq.com/q/d/l/?s=$(lowercase(ticker)).us&i=d"
     io = IOBuffer()
-    Downloads.download(url, io, headers = ["User-Agent" => "Mozilla/5.0"])
-    
+    Downloads.download(url, io)
     seekstart(io)
     return CSV.read(io, DataFrame)
 end
 
 try
-    # init with first iter
-    pricesDf = fetchYahooData(tickers[1], startTime, stopTime)
-    pricesDf = pricesDf[:, [:Date, Symbol("Adj Close")]]
-    rename!(pricesDf, Symbol("Adj Close") => Symbol(tickers[1]))
+    local pricesDf
+    pricesDf = fetchStooqData(tickers[1])
+    pricesDf = pricesDf[:, [:Date, :Close]]
+    rename!(pricesDf, :Close => Symbol(tickers[1]))
 
     for i in 2:length(tickers)
         currentTicker = tickers[i]
         
-        tempDf = fetchYahooData(currentTicker, startTime, stopTime)
-        tempDf = tempDf[:, [:Date, Symbol("Adj Close")]]
-        rename!(tempDf, Symbol("Adj Close") => Symbol(currentTicker))
+        tempDf = fetchStooqData(currentTicker)
+        tempDf = tempDf[:, [:Date, :Close]]
+        rename!(tempDf, :Close => Symbol(currentTicker))
         
         pricesDf = innerjoin(pricesDf, tempDf, on=:Date)
     end
 
     sort!(pricesDf, :Date)
+    pricesDf = last(pricesDf, 252)
     
     priceCols = Symbol.(tickers)
     priceMatrix = Matrix{Float64}(pricesDf[:, priceCols])
     
     returns = diff(log.(priceMatrix), dims=1)
-    
     vols = std(returns, dims=1) .* sqrt(252)
     
     corrMatrix = cor(returns)
@@ -46,10 +41,10 @@ try
     
     spots = priceMatrix[end, :]
 
-    CSV.write("market_vols.csv", DataFrame(vols = vec(vols)), header=false)
-    CSV.write("market_spots.csv", DataFrame(spots = vec(spots)), header=false)
-    CSV.write("market_correlation.csv", Tables.table(corrMatrix), header=false)
+    CSV.write("../data/market_vols.csv", DataFrame(vols = vec(vols)), header=false)
+    CSV.write("../data/market_spots.csv", DataFrame(spots = vec(spots)), header=false)
+    CSV.write("../data/market_correlation.csv", Tables.table(corrMatrix), header=false)
 
 catch e
-    @error "Data fetch failed." exception=(e, catch_backtrace())
+    @error "data fetch failed" exception=(e, catch_backtrace())
 end
