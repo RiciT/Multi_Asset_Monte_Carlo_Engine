9486586d235d1bf03feba6562ec039338f3598f4
RiCiT <tamas.balint.farago@gmail.com>
2026-02-20 11:56:31 +0100

    removed first test and added a pricer class to calculate average payoff

---
+++ src/Pricer.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "MultiAssetSimulator.hpp"
+
+class BasketCallOption {
+public:
+    double calculateBasketCallPrice(int numPaths, double strike, double T, double r, const std::vector<Asset>& basket)
+    {
+        double totalPayoff = 0.0;
+
+        for (auto p = 0; p < numPaths; ++p)
+        {
+            std::vector<double> terminalPrices = simulator.generatePath(basket);
+
+            double averagePrice = 0.0;
+            for (auto prices : terminalPrices) averagePrice += prices;
+            averagePrice /= basket.size();
+
+            //standard call payoff is: max(avg - strikePrice, 0)
+            double payoff = std::max(averagePrice - strike, 0.0);
+            totalPayoff += payoff;
+        }
+
+        //average and discount back to present value
+        double averagePayoff = totalPayoff / numPaths;
+        //using formula V_0 * e^{-rT}
+        return std::exp(-r * T) * averagePayoff;
+    }
+private:
+    MultiAssetSimulator simulator;
+};
